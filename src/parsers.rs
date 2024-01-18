35c5d42deb20c5e87055c400ad5f50c54368e2e3
miampf <miampf@proton.me>
2024-01-18 23:56:31 +0100

    implemented function that constructs AST

--- src/parsers.rs
+++ src/parsers.rs
@@ -9,13 +9,68 @@ pub mod tagline {
 }
 
 pub mod query {
+    use pest::{iterators::Pairs, pratt_parser::PrattParser};
     use pest_derive::Parser;
+
+    /// Expr represents an AST for a search query.
+    #[derive(Debug)]
+    pub enum Expr {
+        Bool(bool),
+        Operation {
+            lhs: Box<Expr>,
+            op: Op,
+            rhs: Box<Expr>,
+        },
+    }
+
+    /// Op is an Operation that can be used in a query.
+    #[derive(Debug)]
+    pub enum Op {
+        And,
+        Or,
+    }
+
+    lazy_static::lazy_static! {
+        static ref PRATT_PARSER: PrattParser<Rule> = {
+            use pest::pratt_parser::{Assoc::*, Op};
+            use Rule::*;
+
+            PrattParser::new()
+                // & and | are evaluated with the same precedence
+                .op(Op::infix(and, Left) | Op::infix(or, Left))
+        };
+    }
 
     #[derive(Parser)]
     #[grammar = "query.pest"]
     /// QueryParser is responsible for parsing the search query.
     /// The relevant rule is `tagsearch`.
     pub struct QueryParser;
+
+    /// construct_query_ast() creates an AST from a string of symbols
+    /// lexed by the QueryParser and a list of tags.
+    pub fn construct_query_ast(pairs: Pairs<Rule>, tags: Vec<&str>) -> Expr {
+        PRATT_PARSER
+            .map_primary(|primary| match primary.as_rule() {
+                Rule::tag => Expr::Bool(tags.contains(&primary.as_str().trim())),
+                Rule::expr => construct_query_ast(primary.into_inner(), tags.clone()),
+                rule => unreachable!("Expected tag, found {:?}", rule),
+            })
+            .map_infix(|lhs, op, rhs| {
+                let op = match op.as_rule() {
+                    Rule::or => Op::Or,
+                    Rule::and => Op::And,
+                    rule => unreachable!("Expected operation, found {:?}", rule),
+                };
+
+                Expr::Operation {
+                    lhs: Box::new(lhs),
+                    op,
+                    rhs: Box::new(rhs),
+                }
+            })
+            .parse(pairs)
+    }
 }
 
 #[cfg(test)]
