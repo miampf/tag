aa76c16afc00eb9d33e6a7cc71cc7dd82653ae0f
miampf <miampf@proton.me>
2024-01-19 14:45:07 +0000

    feat: Cli (#16)

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,54 @@ use tag::{
     search::get_tags_from_files,
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tagged_files = get_tags_from_files("testfiles")?;
+mod cli {
+    use clap::Parser;
 
-    for file in tagged_files.iter() {
-        println!("File {} contains {:?}", file.path.display(), file.tags);
+    #[derive(Parser)]
+    #[command(author, version, about, long_about = None)]
+    pub struct Cli {
+        #[clap(value_name = "QUERY")]
+        /// Search query for the tags
+        pub query: String,
+
+        #[clap(value_name = "PATH")]
+        /// The path that will be searched
+        pub path: String,
+
+        #[arg(short, long)]
+        /// Only print the paths of matched files.
+        pub silent: bool,
     }
 
-    let ast = construct_query_ast(
-        QueryParser::parse(Rule::tagsearch, "!#e")
-            .unwrap()
-            .next()
-            .unwrap()
-            .into_inner(),
-        vec!["#a", "#c", "#d"],
-    );
+    impl Cli {
+        pub fn new_and_parse() -> Cli {
+            Cli::parse()
+        }
+    }
+}
 
-    println!("{:#?}", ast);
-    println!("{}", evaluate_ast(ast));
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = cli::Cli::new_and_parse();
+
+    let file_index = get_tags_from_files(args.path.as_str())?;
+    let query = QueryParser::parse(Rule::tagsearch, args.query.as_str())?;
+
+    for file in file_index.iter() {
+        let ast = construct_query_ast(
+            query.clone().next().unwrap().into_inner(),
+            file.tags.iter().map(|tag| tag.as_str()).collect(),
+        );
+
+        if !evaluate_ast(ast) {
+            continue;
+        }
+
+        println!("{}", file.path.display());
+
+        if !args.silent {
+            println!("\ttags:{:?} ", file.tags);
+        }
+    }
 
     Ok(())
 }
